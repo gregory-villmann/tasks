eb47cabd55e5ad0b8996a90ae5912c1b3bc1e763
Gregory Villmann <gregory.villmann@khk.ee>
2021-10-22 16:11:02 +0300

    implemented deletion of a task

--- app.js
+++ app.js
@@ -9,7 +9,13 @@ ul.appendChild(li);
 }*/
 
 const form = document.querySelector('form');
+
+
+const taskList = document.querySelector(".collection");
+
 form.addEventListener('submit', addTask);
+
+taskList.addEventListener('click', delItem);
 
 
 function addTask(event){
@@ -21,10 +27,29 @@ function addTask(event){
     // add list item a class name so it would get the styling
     li.className = 'collection-item';
 
+
+    // <li class="collection-item">test vms <span class="deleteItem"><a onclick="delItem()">X</a></span></li>
+
+    const link = document.createElement('a');
+     link.className = "secondary-content";
+     link.setAttribute('href', "#");
+     link.appendChild(document.createTextNode('X'));
+     li.appendChild(link);
+
+
     li.appendChild(document.createTextNode(taskInput));
     const ul = document.querySelector(".collection");
     ul.appendChild(li);
 
 
     event.preventDefault();
+}
+
+function delItem(e){
+    const ul = document.querySelector(".collection");
+    if(e.target.textContent == "X"){
+        if(confirm("Sure you want to delete")){
+        ul.removeChild(e.target.parentElement);
+        }
+    }
 }
